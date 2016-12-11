e211aa990eedd676fa4975f25f98fd3b1702749c
Frans Peter Larsen <larsen.frans@gmail.com>
2016-12-11 17:24:06 +0100

    Animations, particles, piles-ish

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -53,19 +53,17 @@ public class Movement : MonoBehaviour {
         }
 
         Collider[] objs = Physics.OverlapSphere(transform.position, itemSearchRadius);
-        GameObject nearbyPile = null;
 
         if(objs.Length > 0) { 
             float smallestDist = float.MaxValue - 1;
-            GameObject foundItem = null;
+            GameObject nearestItem = null;
 
 
             for (int i = 0; i < objs.Length; i++)
             {
                 if(objs[i].CompareTag("Pile"))
                 {
-                    nearbyPile = objs[i].gameObject;
-                    continue;
+                    nearestItem = objs[i].gameObject;
                 }
 
                 if (!objs[i].CompareTag("Item") || holdingItem == objs[i])
@@ -75,13 +73,13 @@ public class Movement : MonoBehaviour {
                 if (dist < smallestDist)
                 {
                     smallestDist = dist;
-                    foundItem = objs[i].gameObject;
+                    nearestItem = objs[i].gameObject;
                 }
             }
 
-            if(foundItem && foundItem != holdingItem)
+            if((nearestItem && nearestItem != holdingItem))
             {
-                marker.transform.position = foundItem.transform.position;
+                marker.transform.position = nearestItem.transform.position;
                 marker.SetActive(true);
             }
             else
@@ -91,44 +89,44 @@ public class Movement : MonoBehaviour {
 
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                if (holdingItem && foundItem && holdingItem != foundItem)
+                if (holdingItem && nearestItem && nearestItem.GetComponent<Pile>())
                 {
+                    Pile p = nearestItem.GetComponent<Pile>();
+                    p.AddStick(holdingItem);
                     holdingItem.GetComponent<Rigidbody>().isKinematic = false;
+                    holdingItem.GetComponent<Rigidbody>().detectCollisions = true;
+                    holdingItem.GetComponent<BoxCollider>().enabled = true;
                     holdingItem.transform.parent = null;
-                    holdingItem.GetComponent<Rigidbody>().velocity = (transform.forward * -5f) + (transform.up * 7f);
-                    holdingItem = null;
-                    holdingItem = foundItem;
-                    foundItem.GetComponent<Rigidbody>().isKinematic = true;
-                    foundItem.transform.parent = hand;
-                    foundItem.transform.position = hand.position;
-                    foundItem.GetComponent<BoxCollider>().enabled = false;
+                    holdingItem.transform.position = nearestItem.transform.position + (Vector3.up * 2);
+                    holdingItem.GetComponent<Rigidbody>().velocity = holdingItem.transform.up * -1;
+
                 }
                 else if (holdingItem)
                 {
                     holdingItem.GetComponent<Rigidbody>().isKinematic = false;
+                    holdingItem.GetComponent<Rigidbody>().detectCollisions = true;
                     holdingItem.GetComponent<BoxCollider>().enabled = true;
                     holdingItem.transform.parent = null;
                     holdingItem.GetComponent<Rigidbody>().velocity = (transform.forward * -5f) + (transform.up * 7f);
+                    if(!holdingItem.GetComponent<Pile>())
+                        holdingItem.AddComponent<Pile>();
                     holdingItem = null;
                 }
                 
-                else if(!holdingItem && foundItem)
+                else if(!holdingItem && nearestItem)
                 {
-                    holdingItem = foundItem;
-                    foundItem.GetComponent<Rigidbody>().isKinematic = true;
-                    foundItem.transform.parent = hand;
-                    foundItem.transform.position = hand.position;
-                    foundItem.GetComponent<BoxCollider>().enabled = false;
-                }
-                if(holdingItem && nearbyPile)
-                {
-
+                    holdingItem = nearestItem;
+                    nearestItem.GetComponent<Rigidbody>().isKinematic = true;
+                    nearestItem.GetComponent<Rigidbody>().detectCollisions = false;
+                    nearestItem.transform.parent = hand;
+                    nearestItem.transform.position = hand.position;
+                    nearestItem.GetComponent<BoxCollider>().enabled = false;
                 }
             }
         }
     }
 
-    void OnGizmosDraw()
+    void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(transform.position, itemSearchRadius);
     }
---
+++ Assets/Scripts/Pile.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Pile : MonoBehaviour {
+    public List<GameObject> sticks;
+    public Billboard countSign;
+
+	// Use this for initialization
+	void Start () {
+        sticks = new List<GameObject>();
+
+        countSign.gameObject.SetActive(false);
+	}
+
+    void Update()
+    {
+        if (!countSign.gameObject.activeSelf && sticks.Count > 1)
+        {
+            countSign.gameObject.SetActive(true);
+        } else if(countSign.gameObject.activeSelf) {
+            countSign.text.text = sticks.Count + " STICKS";
+        }
+
+        for(int i = 0; i < sticks.Count;i++)
+        {
+            Rigidbody body = sticks[i].GetComponent<Rigidbody>();
+            Collider collider = sticks[i].GetComponent<Collider>();
+            if(body.velocity.magnitude < 0.5)
+            {
+                body.isKinematic = true;
+            }
+        }
+    }
+
+    // Make script that determines if this is becoming a pile a not
+
+    public void AddStick(GameObject stick)
+    {
+        sticks.Add(stick);
+    }
+
+    void OnDrawGizmos()
+    {
+
+    }
+}

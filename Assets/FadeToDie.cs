932f7b3b1300a46151a6c4da60ae0b521ee1b0ed
Frans Peter Larsen <larsen.frans@gmail.com>
2016-12-14 22:12:31 +0100

    Added footprints

---
+++ Assets/FadeToDie.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeToDie : MonoBehaviour {
+    public float timeToStartFade = 0f;
+    public float timeToDeath = 1f;
+
+    private Material material;
+    private Color originalColor;
+    private Color fadingColor;
+    private float step;
+
+	// Use this for initialization
+	void Start () {
+        material = GetComponent<MeshRenderer>().material;
+        originalColor = material.color;
+        fadingColor = material.color;
+        fadingColor.a = 0f;
+        step = 0f;
+
+        StartCoroutine(Die());
+	}
+
+    private IEnumerator Die()
+    {
+        yield return new WaitForSeconds(timeToStartFade);
+
+        while(step < 1f)
+        {
+            material.color = Color.Lerp(originalColor, fadingColor, step);
+
+            step += (1/timeToDeath) * Time.deltaTime;
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        Destroy(gameObject);
+    }
+}
--- Assets/Feets.cs
+++ Assets/Feets.cs
@@ -2,19 +2,19 @@
 using System.Collections;
 
 public class Feets : MonoBehaviour {
+    public Transform player;
     public AudioClip clip;
+    public GameObject footprint;
 
 	// Use this for initialization
 	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
 	}
 
     void OnTriggerEnter(Collider other)
     {
         AudioSource.PlayClipAtPoint(clip, transform.position);
+
+        Instantiate(footprint, transform.position + (Vector3.up * 0.01f), footprint.transform.rotation * Quaternion.Euler(1f,1f,-player.transform.eulerAngles.y));
     }
 }
